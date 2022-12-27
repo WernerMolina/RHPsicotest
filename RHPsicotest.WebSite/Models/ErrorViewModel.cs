ec720b5b4a06fe89f4e9ccd42f0eefe13d677c5e
Werner Molina <Wmolina1225@hotmail.com>
2022-12-27 15:44:05 -0600

    Proyecto Web MVC creado

---
+++ RHPsicotest.WebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RHPsicotest.WebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

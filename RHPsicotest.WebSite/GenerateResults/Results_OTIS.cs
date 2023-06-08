e73aa66901c3f3184f701193c073f4c5c35f0df2
Werner Molina <Wmolina1225@gmail.com>
2023-06-08 16:01:20 -0600

    Modificaciones en el test otis

--- RHPsicotest.WebSite/GenerateResults/Results_OTIS.cs
+++ RHPsicotest.WebSite/GenerateResults/Results_OTIS.cs
@@ -13,7 +13,7 @@ namespace RHPsicotest.WebSite.GenerateResults
 
             byte counter = 0;
 
-            foreach (var response in responsesOTIS)
+            foreach (Responses_OTIS response in responsesOTIS)
             {
                 if (candidateResponses[counter] == response.Correct) scoreTotal++;
 
@@ -25,44 +25,42 @@ namespace RHPsicotest.WebSite.GenerateResults
 
         public static byte GetCIByScore(byte score)
         {
-            byte IQ = 0;
+            if (score == 75) return 127;
+            if (score == 74) return 126;
+            if (score == 73) return 125;
+            if (score == 72) return 124;
+            if (score >= 70) return 122;
+            if (score >= 68) return 120;
+            if (score >= 66) return 118;
+            if (score == 65) return 116;
+            if (score >= 63) return 114;
+            if (score >= 61) return 112;
+            if (score >= 59) return 110;
+            if (score >= 57) return 108;
+            if (score >= 55) return 106;
+            if (score >= 53) return 104;
+            if (score >= 51) return 102;
+            if (score >= 49) return 100;
+            if (score >= 47) return 98;
+            if (score >= 45) return 96;
+            if (score >= 43) return 94;
+            if (score >= 41) return 92;
+            if (score >= 39) return 90;
+            if (score >= 37) return 88;
+            if (score >= 35) return 86;
+            if (score >= 33) return 84;
+            if (score == 32) return 82;
+            if (score >= 30) return 80;
+            if (score >= 28) return 78;
+            if (score >= 26) return 76;
+            if (score >= 24) return 74;
+            if (score >= 22) return 72;
+            if (score >= 20) return 70;
+            if (score >= 18) return 68;
+            if (score == 17) return 66;
+            if (score >= 0) return 65;
 
-            if (score == 75) IQ = 127;
-            if (score == 74) IQ = 126;
-            if (score == 73) IQ = 125;
-            if (score == 72) IQ = 124;
-            if (score >= 70) IQ = 122;
-            if (score >= 68) IQ = 120;
-            if (score >= 66) IQ = 118;
-            if (score == 65) IQ = 116;
-            if (score >= 63) IQ = 114;
-            if (score >= 61) IQ = 112;
-            if (score >= 59) IQ = 110;
-            if (score >= 57) IQ = 108;
-            if (score >= 55) IQ = 106;
-            if (score >= 53) IQ = 104;
-            if (score >= 51) IQ = 102;
-            if (score >= 49) IQ = 100;
-            if (score >= 47) IQ = 98;
-            if (score >= 45) IQ = 96;
-            if (score >= 43) IQ = 94;
-            if (score >= 41) IQ = 92;
-            if (score >= 39) IQ = 90;
-            if (score >= 37) IQ = 88;
-            if (score >= 35) IQ = 86;
-            if (score >= 33) IQ = 84;
-            if (score == 32) IQ = 82;
-            if (score >= 30) IQ = 80;
-            if (score >= 28) IQ = 78;
-            if (score >= 26) IQ = 76;
-            if (score >= 24) IQ = 74;
-            if (score >= 22) IQ = 72;
-            if (score >= 20) IQ = 70;
-            if (score >= 18) IQ = 68;
-            if (score == 17) IQ = 66;
-            if (score <= 16) IQ = 65;
-
-            return IQ;
+            return 0;
         }
 
         public static string GetDescriptionByCI(byte IQ)
